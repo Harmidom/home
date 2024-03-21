e6fc4c5817b37bd7dd21e5c4d3392177e82ba08e
Harmidom <163033084+Harmidom@users.noreply.github.com>
2024-03-21 20:18:57 +0100

    Create czy_masz_dziewczyne.cpp

---
+++ czy_masz_dziewczyne.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+string wartosc;
+
+int main()
+{
+    cout << "Czy masz dziewczynę" << endl;
+    
+    cin >> wartosc;
+    
+    if(wartosc == "tak")
+    {
+        cout << "Gratulacje!";
+    }
+    
+    if (wartosc == "nie")
+    {
+      cout << "Jak miałeś to try again, a jak nie to szkoda, wez się do roboty i se znajdź kurde no."<< endl;
+    }
+    
+    cout << "if u bored watch this crap: https://www.youtube.com/results?search_query=skibidi+toilet                            xd" << endl;
+    cout << "A wiesz co to jest?" << endl;
+    
+    cin >> wartosc;
+
+    if (wartosc == "tak")
+    {
+        cout << "to zapomnij";
+    }
+
+    if (wartosc == "nie")
+    {
+        cout << "to polecam zajrzec hahahahahaha";
+    }
+    
+
+    return 0;
+}
